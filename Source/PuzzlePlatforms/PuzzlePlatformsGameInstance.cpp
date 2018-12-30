5c65ecf9640eae5f7e36e0b96414a082d75667a7
Giovanni Giacomo <giovanni@unitool.com.br>
2018-12-30 18:30:36 -0200

    Added LoadMenu & MainMenu

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -24,6 +24,16 @@ void UPuzzlePlatformsGameInstance::Init()
 	UE_LOG(LogTemp, Warning, TEXT("Found class %s"), *MenuClass->GetName());
 }
 
+void UPuzzlePlatformsGameInstance::LoadMenu()
+{
+	if (!ensure(MenuClass)) return;
+
+	UUserWidget* Menu = CreateWidget<UUserWidget>(this, MenuClass);
+	if (!ensure(Menu)) return;
+
+	Menu->AddToViewport();
+}
+
 void UPuzzlePlatformsGameInstance::Host()
 {
 	UEngine* Engine = GetEngine();
--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
@@ -19,6 +19,9 @@ public:
 
 	virtual void Init() override;
 
+	UFUNCTION(BlueprintCallable, Category=Menu)
+	void LoadMenu();
+
 	UFUNCTION(Exec)
 	void Host();
 
